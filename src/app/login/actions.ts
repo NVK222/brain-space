89dd02f601568e61883944f7d481071f6336f3cb
NVK222 <neerajvk2004@gmail.com>
2026-01-21 18:16:50 +0530

    Added login/signup page

---
+++ src/app/login/actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function login(formData: FormData) {
+  const supabase = await createClient();
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+  if (error) {
+    return redirect("/login?error=true");
+  }
+
+  revalidatePath("/dashboard", "layout");
+  redirect("/dashboard");
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient();
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const { error } = await supabase.auth.signUp({ email, password });
+
+  if (error) {
+    return redirect("/login?error=true");
+  }
+  revalidatePath("/login", "layout");
+  redirect("/login");
+}
